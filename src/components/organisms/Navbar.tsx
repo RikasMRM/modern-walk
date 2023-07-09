347a90af1fae1524473a5554829db882731ed30d
RikasMRM <rikasrkf@gmail.com>
2023-07-09 22:42:38 +0530

    Code revamp

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -4,7 +4,7 @@ import Title from "../atoms/Title";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-white p-2 mt-0 w-full shadow-md">
+    <nav className="bg-white p-2 mt-0 w-full mb-3 shadow-md">
       <div className="container mx-auto flex items-center justify-center">
         <Link to="/" className="text-gray-800 text-center text-3xl font-bold">
           <Title children={"Modern Walk"} />
