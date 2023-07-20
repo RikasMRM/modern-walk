d56f7a151d38af5fe9e039e42032b1a852856365
RikasMRM <rikasrkf@gmail.com>
2023-07-20 13:22:15 +0530

    feature user api code revamp

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+const API_CLIENT_URL = "http://localhost:3000"
+
 export const productApi = axios.create({
   baseURL: "https://fakestoreapi.com",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const clientApi = axios.create({
+  baseURL: API_CLIENT_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,12 +1,8 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:3000",
-});
+import { clientApi } from "./api.service";
 
 export const registerUser = async (userData: any) => {
   try {
-    const response = await api.post("/users", userData);
+    const response = await clientApi.post("/users", userData);
     return response.data;
   } catch (error) {
     console.error("Error registering user:", error);
@@ -16,7 +12,7 @@ export const registerUser = async (userData: any) => {
 
 export const loginUser = async (userData: { email: any; password: any; }) => {
   try {
-    const response = await api.get("/users", {
+    const response = await clientApi.get("/users", {
       params: {
         email: userData.email,
         password: userData.password,
